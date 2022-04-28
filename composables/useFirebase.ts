2123663df450c1f78cca95029c32a75d6f74ac35
Jeff Delaney <hello@fireship.io>
2022-04-28 16:24:55 -0700

    rm config

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 
 export const useFirebase = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyCLXaUcLfhz2RH8Dz6gESgRfMS0mQYzOhc",
-        authDomain: "instafire-app.firebaseapp.com",
-        databaseURL: "https://instafire-app.firebaseio.com",
-        projectId: "instafire-app",
-        storageBucket: "instafire-app.appspot.com",
-        messagingSenderId: "349272671197",
-        appId: "1:349272671197:web:66fbd923a8dd82d9f6ec34"
+        // your firebase config
     };
 
     const firebaseApp = initializeApp(firebaseConfig);
